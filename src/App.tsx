4d0372d3d7f02cc9638310043f5d654a0062f8eb
svndor-hub <sanzhar.d3@gmail.com>
2025-04-22 14:59:26 +0500

    remove import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { ReactNode } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
